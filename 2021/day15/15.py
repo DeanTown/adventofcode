fbb016f90b77b9cd56c2a07bcc8e143979e1d55d
DeanTown <oliver.reckordgroten@gmail.com>
2022-11-22 16:30:43 -0500

    I think the adjacency list is building correctly...

--- 2021/day15/15.py
+++ 2021/day15/15.py
@@ -1,3 +1,23 @@
 from lib.utils import read_input
+from lib.graph import Graph
 
-print(read_input("2021/day15/input.txt"))
+input = read_input("2021/day15/input.txt")
+# We will need both the individual lines as well as the whole string of numbers
+lines = input.split('\n')
+input = input.replace('\n', '')
+line_len = len(lines[0])
+input_len = len(input)
+
+graph = Graph(input_len)
+
+"""
+Convert the input into an adjacency list
+"""
+for i in range(input_len):
+    directions = [i-line_len, i+line_len, i-1, i+1]
+    for dir in directions:
+        if dir >=0 and dir < input_len:
+            graph.add_edge(dir, i, int(input[i]))
+
+graph.print_graph()
+        
