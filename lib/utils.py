ff8b2f43ec51e56721c22f6588c1b99ddf6ada84
DeanTown <oliver.reckordgroten@gmail.com>
2022-11-22 16:27:10 -0500

    have read_input return the unedited file

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,3 @@
 def read_input(filename):
     file = open(filename, 'r')
-    lines = file.readlines()
-    stripped_lines = []
-    for line in lines:
-        stripped_lines.append(line.strip())
-    return stripped_lines
+    return file.read()
