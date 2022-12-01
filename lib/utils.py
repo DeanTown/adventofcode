e4f2487ae6ed7360414245e52b3b314c5a401edd
DeanTown <oliver.reckordgroten@gmail.com>
2022-12-01 10:48:29 -0500

    refine read_input()

--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,5 @@
-def read_input(filename):
+def read_input(filename, split_lines=False):
     file = open(filename, 'r')
+    if split_lines:
+        return file.read().splitlines()
     return file.read()
