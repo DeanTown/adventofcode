0d2bf9bcc9abcf926d8665864390fc4e44bbaaa4
DeanTown <oliver.reckordgroten@gmail.com>
2022-12-01 11:38:15 -0500

    Rename folders so they don't start with a number. Rework structure of day scripts to support unit testing

---
+++ y2022/day1/go.py
@@ -0,0 +1,28 @@
+from lib.utils import read_input
+
+def go():
+
+    input = read_input("y2022/day1/input.txt", split_lines=True)
+
+    # PART 1
+
+    calorie_counts = [0]
+    for line in input:
+        if line == '':
+            calorie_counts.append(0)
+        else:
+            calorie_counts[-1] += int(line)
+
+    part1 = (max(calorie_counts))
+
+    # PART 2
+
+    sorted_counts = sorted(calorie_counts, reverse=True)[:3]
+    part2 = (sum(sorted_counts))
+
+    return part1, part2
+
+
+if __name__ == "__main__":
+    part1, part2 = go()
+    print(part1, part2)
