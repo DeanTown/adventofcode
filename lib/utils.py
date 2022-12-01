2ce64e274b0e4c12034baca462ad832660d98216
DeanTown <oliver.reckordgroten@gmail.com>
2022-12-01 14:27:22 -0500

    update read_input

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,10 @@
-def read_input(filename, split_lines=False):
+def read_input(filename, split_lines=False, convert_to_num=False):
     file = open(filename, 'r')
     read = file.read()
     file.close()
     if split_lines:
         return read.splitlines()
+    if convert_to_num:
+        lines = read.splitlines()
+        return [int(line) for line in lines]
     return read
