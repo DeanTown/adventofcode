35c12707179ce85cfa253e5a65444bd97f926764
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2022-12-03 16:15:56 -0500

    2022:3

--- tests/test_2022.py
+++ tests/test_2022.py
@@ -1,6 +1,6 @@
 import unittest
 
-from y2022 import day1, day2
+from y2022 import day1, day2, day3
 
 class Test2022(unittest.TestCase):
 
@@ -13,3 +13,8 @@ class Test2022(unittest.TestCase):
         results = day2.go()
         self.assertEqual(results[0], 13005)
         self.assertEqual(results[1], 11373)
+
+    def test_day3(self):
+        results = day3.go()
+        self.assertEqual(results[0], 7727)
+        self.assertEqual(results[1], 2609)
---
+++ y2022/day3.py
@@ -0,0 +1,46 @@
+import string as s
+from lib.utils import read_input
+
+def go():
+
+    input = read_input("input/2022:3.txt", split_lines=True)
+
+    # PART 1
+
+    split = [[line[:len(line)//2], line[len(line)//2:]] for line in input]
+    duplicates = find_duplicates(split)
+    part1 = calculate_priority(duplicates)
+
+    # PART 2
+
+    split = [input[i:i+3] for i in range(0, len(input), 3)]
+    duplicates = find_duplicates(split)
+    part2 = calculate_priority(duplicates)
+
+    return part1, part2
+
+def find_duplicates(split):
+    duplicates = []
+    import ipdb
+    ipdb.set_trace()
+    for first, *rest in split:
+        while True:
+            char = first[0]
+            if all(char in string for string in rest):
+                duplicates.append(char)
+                break
+            else:
+                first = list(filter(lambda x: x != char, first))
+    return duplicates
+
+def calculate_priority(chars):
+    priority_map = {c: i+1 for i, c in enumerate(list(s.ascii_lowercase) + list(s.ascii_uppercase))}
+    priority = 0
+    for char in chars:
+        priority += priority_map[char]
+    return priority
+
+
+if __name__ == "__main__":
+    part1, part2 = go()
+    print(part1, part2)
