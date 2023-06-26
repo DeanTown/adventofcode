445c7663f68944ce0de7bc39b2bb331094280125
DeanTown <oliver.reckordgroten@gmail.com>
2023-06-26 14:58:46 -0400

    update unit testing imports p2 and use proper unit testing syntax

--- tests/test_2021.py
+++ tests/test_2021.py
@@ -1,15 +1,17 @@
 import unittest
 
-import y2021
+from y2021 import day1, day2
 
 
 class Test2021(unittest.TestCase):
+    # @unittest.skip("Skip!")
     def test_day1(self):
-        results = y2021.day1.go()
+        results = day1.go()
         self.assertEqual(results[0], 1791)
         self.assertEqual(results[1], 1822)
 
+    # @unittest.skip("Skip!")
     def test_day2(self):
-        results = y2021.day2.go()
+        results = day2.go()
         self.assertEqual(results[0], 2039256)
         self.assertEqual(results[1], 1856459736)
--- tests/test_2022.py
+++ tests/test_2022.py
@@ -1,63 +1,74 @@
 import unittest
 
-import y2022
+from y2022 import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11
 
 
 class Test2022(unittest.TestCase):
+    # @unittest.skip("Skip!")
     def test_day1(self):
-        results = y2022.day1.go()
+        results = day1.go()
         self.assertEqual(results[0], 68923)
         self.assertEqual(results[1], 200044)
 
+    # @unittest.skip("Skip!")
     def test_day2(self):
-        results = y2022.day2.go()
+        results = day2.go()
         self.assertEqual(results[0], 13005)
         self.assertEqual(results[1], 11373)
 
+    # @unittest.skip("Skip!")
     def test_day3(self):
-        results = y2022.day3.go()
+        results = day3.go()
         self.assertEqual(results[0], 7727)
         self.assertEqual(results[1], 2609)
 
+    # @unittest.skip("Skip!")
     def test_day4(self):
-        results = y2022.day4.go()
+        results = day4.go()
         self.assertEqual(results[0], 487)
         self.assertEqual(results[1], 849)
 
+    # @unittest.skip("Skip!")
     def test_day5(self):
-        results = y2022.day5.go()
+        results = day5.go()
         self.assertEqual(results[0], "BZLVHBWQF")
         self.assertEqual(results[1], "TDGJQTZSL")
 
+    # @unittest.skip("Skip!")
     def test_day6(self):
-        results = y2022.day6.go()
+        results = day6.go()
         self.assertEqual(results[0], 1356)
         self.assertEqual(results[1], 2564)
 
+    # @unittest.skip("Skip!")
     def test_day7(self):
-        results = y2022.day7.go()
+        results = day7.go()
         self.assertEqual(results[0], 1325919)
         self.assertEqual(results[1], 2050735)
 
+    # @unittest.skip("Skip!")
     def test_day8(self):
-        results = y2022.day8.go()
+        results = day8.go()
         self.assertEqual(results[0], 1835)
         self.assertEqual(results[1], 263670)
 
-    # def test_day9(self):
-    #     results = y2022.day9.go()
-    #     self.assertEqual(results[0], False)
-    #     self.assertEqual(results[1], False)
+    @unittest.skip("Skip!")
+    def test_day9(self):
+        results = day9.go()
+        self.assertEqual(results[0], False)
+        self.assertEqual(results[1], False)
 
+    # @unittest.skip("Skip!")
     def test_day10(self):
-        results = y2022.day10.go()
+        results = day10.go()
         self.assertEqual(results[0], 14360)
         self.assertEqual(
             results[1],
             "###...##..#..#..##..####.###..####.####.#..#.#..#.#.#..#..#.#....#..#.#.......#.###..#....##...#..#.###..#..#.###....#..#..#.#.##.#.#..####.#....###..#.....#...#..#.#..#.#.#..#..#.#....#.#..#....#....###...###.#..#.#..#.####.#..#.####.####.",
         )
 
+    # @unittest.skip("Skip!")
     def test_day11(self):
-        results = y2022.day11.go()
+        results = day11.go()
         self.assertEqual(results[0], 151312)
         self.assertEqual(results[1], False)
